30cfa854bd954919a9a0ccd7c2e05c3a6ca34fd8
Agustin Silva <silva.agustinn@gmail.com>
2022-02-01 12:22:43 -0300

    6.2 - Generic Client, Behaviours and Callbacks

---
+++ lib/poeticoins/exchanges/bitstamp_client.ex
@@ -0,0 +1,78 @@
+defmodule Poeticoins.Exchanges.BitstampClient do
+  alias Poeticoins.{Trade, Product}
+  alias Poeticoins.Exchanges.Client
+
+  import Client, only: [validate_required: 2]
+
+  @behaviour Client
+
+  @impl true
+  def exchange_name, do: "bitstamp"
+
+  @impl true
+  def server_host, do: 'ws.bitstamp.net'
+
+  @impl true
+  def server_port, do: 443
+
+  @impl true
+  def handle_ws_message(%{"event" => "trade"} = msg, state) do
+    _trade =
+      msg
+      |> message_to_trade()
+      |> IO.inspect(label: "trade")
+
+    {:noreply, state}
+  end
+
+  def handle_ws_message(msg, state) do
+    IO.inspect(msg, label: "unhandled message")
+    {:noreply, state}
+  end
+
+  @impl true
+  def subscription_frames(currency_pairs) do
+    Enum.map(currency_pairs, &subscription_frame/1)
+  end
+
+  def subscription_frame(currency_pair) do
+    msg = %{
+      "event" => "bts:subscribe",
+      "data" => %{
+        "channel" => "live_trades_#{currency_pair}"
+      }
+    } |> Jason.encode!()
+
+    {:text, msg}
+  end
+
+  @spec message_to_trade(map()) :: {:ok, Trade.t()} | {:error, any()}
+  def message_to_trade(%{"data" => data, "channel" => "live_trades_" <> currency_pair} = msg) when is_map(data) do
+    with :ok <- validate_required(msg, ["amount_str", "price_str", "timestamp"]),
+         {:ok, traded_at} <- timestamp_to_datetime(data["timestamp"])
+    do
+      Trade.new(
+        product: Product.new(exchange_name(), currency_pair),
+        price: data["price_str"],
+        volume: data["amount_str"],
+        traded_at: traded_at
+      )
+    else
+      {:error, _reason} = error ->
+        error
+    end
+  end
+
+  def message_to_trade(_msg), do: {:error, :invalid_trade_message}
+
+  @spec timestamp_to_datetime(String.t()) :: {:ok, DateTime.t()} | {:error, atom()}
+  defp timestamp_to_datetime(ts) do
+    case Integer.parse(ts) do
+      {timestamp, _} ->
+        DateTime.from_unix(timestamp)
+
+      :error ->
+        {:error, :invalid_timestamp_string}
+    end
+  end
+end
--- lib/poeticoins/exchanges/coinbase_client.ex
+++ lib/poeticoins/exchanges/coinbase_client.ex
@@ -1,51 +1,23 @@
 defmodule Poeticoins.Exchanges.CoinbaseClient do
-  use GenServer
-
   alias Poeticoins.{Trade, Product}
+  alias Poeticoins.Exchanges.Client
 
-  @exchange_name "coinbase"
+  import Client, only: [validate_required: 2]
 
-  def start_link(currency_pairs, options \\ []) do
-    GenServer.start_link(__MODULE__, currency_pairs, options)
-  end
+  @behaviour Client
 
-  def init(currency_pairs) do
-    state = %{
-      currency_pairs: currency_pairs,
-      conn: nil
-    }
+  @impl true
+  def exchange_name, do: "coinbase"
 
-    {:ok, state, {:continue, :connect}}
-  end
-
-  def handle_continue(:connect, state) do
-    {:noreply, connect(state)}
-  end
-
+  @impl true
   def server_host, do: 'https://ws-feed.pro.coinbase.com/'
+
+  @impl true
   def server_port, do: 443
 
-  def connect(state) do
-    {:ok, conn} = :gun.open(server_host(), server_port(), %{protocols: [:http]})
-    %{state | conn: conn}
-  end
-
-  def handle_info({:gun_up, conn, :http}, %{conn: conn} = state) do
-    :gun.ws_upgrade(conn, "/")
-    {:noreply, state}
-  end
-
-  def handle_info({:gun_upgrade, conn, _ref, ["websocket"], _headers}, %{conn: conn} = state) do
-    subscribe(state)
-    {:noreply, state}
-  end
-
-  def handle_info({:gun_ws, conn, _ref, {:text, msg} = _frame}, %{conn: conn} = state) do
-    handle_ws_message(Jason.decode!(msg), state)
-  end
-
+  @impl true
   def handle_ws_message(%{"type" => "ticker"} = msg, state) do
-    trade =
+    _trade =
       msg
       |> message_to_trade()
       |> IO.inspect(label: "trade")
@@ -54,7 +26,19 @@ defmodule Poeticoins.Exchanges.CoinbaseClient do
   end
 
   def handle_ws_message(msg, state) do
+    IO.inspect(msg, label: "unhandled message")
     {:noreply, state}
+  end
+
+  @impl true
+  def subscription_frames(currency_pairs) do
+    msg = %{
+      "type" => "subscribe",
+      "product_ids" => currency_pairs,
+      "channels" => ["ticker"]
+    } |> Jason.encode!()
+
+    [{:text, msg}]
   end
 
   @spec message_to_trade(map()) :: {:ok, Trade.t()} | {:error, any()}
@@ -64,40 +48,15 @@ defmodule Poeticoins.Exchanges.CoinbaseClient do
     do
       currency_pair = msg["product_id"]
 
-      trade =
-        Trade.new(
-          product: Product.new(@exchange_name, currency_pair),
-          price: msg["price"],
-          volume: msg["last_size"],
-          traded_at: traded_at
-        )
-
-      {:ok, trade}
+      Trade.new(
+        product: Product.new(exchange_name(), currency_pair),
+        price: msg["price"],
+        volume: msg["last_size"],
+        traded_at: traded_at
+      )
     else
       {:error, _reason} = error ->
         error
     end
-  end
-
-  @spec validate_required(map(), [String.t()]) :: :ok | {:error, {String.t(), :required}}
-  def validate_required(msg, keys) do
-    required_key = keys |> Enum.find(&(is_nil(msg[&1])))
-
-    if is_nil(required_key), do: :ok, else: {:error, required_key, :required}
-  end
-
-  defp subscribe(state) do
-    subscription_frames(state.currency_pairs)
-    |> Enum.each(&:gun.ws_send(state.conn, &1))
-  end
-
-  defp subscription_frames(currency_pairs) do
-    msg = %{
-      "type" => "subscribe",
-      "product_ids" => currency_pairs,
-      "channels" => ["ticker"]
-    } |> Jason.encode!()
-
-    [{:text, msg}]
   end
 end
